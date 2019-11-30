80e6f9ccad8a5fbce8be2d007af2d187dae7a5ac
dpaschal-lf <daniel.paschal@gmail.com>
2019-11-29 16:01:53 -0800

    added initial grab of data from gallery

--- src/components/gallery/Gallery.js
+++ src/components/gallery/Gallery.js
@@ -3,6 +3,23 @@ import React from 'react';
 class Gallery extends React.Component{
     constructor(props){
         super(props);
+        this.updateData = this.updateData.bind( this );
+        this.state = {
+            data: []
+        }
+    }
+    componentDidMount(){
+        this.getGalleryData();
+    }
+    getGalleryData(){
+        fetch('api/gallery.php')
+            .then( response => response.json() )
+            .then( this.updateData );
+    }
+    updateData( response ){
+        this.setState({
+            data: response
+        })
     }
     render(){
         return(
