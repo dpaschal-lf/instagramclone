d40ac11a17555e4344111bdd4fed7c5baab265bb
dpaschal-lf <daniel.paschal@gmail.com>
2019-11-29 17:22:53 -0800

    modified gallery item display code

--- src/components/gallery/GalleryItem.js
+++ src/components/gallery/GalleryItem.js
@@ -4,6 +4,16 @@ export default props =>{
     return(
         <div className="galleryItem">
             <img className="background" src={props.data.imagePath} />
+            <div className="icons">
+                <div className="iconContainer">
+                    <i class="fas fa-heart iconBody"></i>
+                    <div className="iconData">3</div>
+                </div>
+                <div className="iconContainer">
+                    <i class="fas fa-comment iconBody"></i>
+                    <div className="iconData">3</div>
+                </div>
+            </div>
         </div>
     );
 }
