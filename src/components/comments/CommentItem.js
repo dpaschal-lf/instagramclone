79738ac5bc5573b802d9b8392a50d9cf2555ea3b
dpaschal-lf <daniel.paschal@gmail.com>
2019-11-30 22:34:39 -0800

    added initial guts to comment item

--- src/components/comments/CommentItem.js
+++ src/components/comments/CommentItem.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
 export default props => {
+    console.log(props.data)
     return (
-        <div>comment</div>
+        <div className="commentItem">
+            <div className="userData">
+                <div className="avatar" style={{backgroundImage: `url(${props.data.avatar})`}}></div>
+                <div className="userName">{props.data.displayName}</div>
+            </div>
+            <div className="messageData">
+                <div className="messageContent">{props.data.message}</div>
+                <div className="messageMetaData">
+                    <div className="messagePosted">{props.data.added}</div>
+                    <div className="MessageEdited">{props.data.edited}</div>
+                </div>
+            </div>
+        </div>
     );
 }
