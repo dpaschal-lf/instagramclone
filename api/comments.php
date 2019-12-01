326c3f6e4bcf2d059140638759715257a67ff599
dpaschal-lf <daniel.paschal@gmail.com>
2019-11-30 22:24:52 -0800

    fixed wrong table type bug in comments

--- api/comments.php
+++ api/comments.php
@@ -7,24 +7,9 @@ require_once('mysql_connect.php');
 if(empty($_GET['postID'])){
     throw new Exception('must supply an id');
 }
+$internalPostID = getIdFromExternalID( 'posts', $_GET['postID'] );
 
-if(!empty($_GET['postID'])){
-    $query = "SELECT p.`id`, p.`userID`, p.`externalID`, p.`likes`, p.`commentCount`, p.`extension`, p.`originalName`, p.`caption`, p.`added`,
-    u.`externalID` as userExternalID, u.`displayName`
-    FROM `posts` AS p
-        JOIN `users` AS u
-            ON u.`id` = p.`userID`
-    WHERE p.`id`=1 AND p.`status`='active'";
-    $result = $db->query($query);
-    if(!$result){
-        throw new Exception('problem with query: '.$db->error);
-    }
-    if($result->num_rows===0){
-        throw new Exception('invalid ID: '. $_GET['id'] );
-    }
-    $postData = $result->fetch_assoc();
-    $postData['imagePath'] = "images/{$postData['userExternalID']}/{$postData['externalID']}.{$postData['extension']}";
-    $postData['comments'] = getCommentsForPost($postData['id']);
-    print(json_encode($postData));
-    exit();
-}
+$data = getCommentsForPost( $internalPostID );
+print(json_encode($data));
+
+?>
