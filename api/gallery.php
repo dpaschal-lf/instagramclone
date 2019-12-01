cdca40180901f3867dfd5f5244a5e736c611ca18
dpaschal-lf <daniel.paschal@gmail.com>
2019-11-30 16:56:38 -0800

    added code for post details

--- api/gallery.php
+++ api/gallery.php
@@ -10,20 +10,20 @@ if(!empty($_GET['id'])){
     FROM `posts` AS p
         JOIN `users` AS u
             ON u.`id` = p.`userID`
-    WHERE p.`id`=1 AND p.`status`='active'"
+    WHERE p.`id`=1 AND p.`status`='active'";
     $result = $db->query($query);
     if(!$result){
         throw new Exception('problem with query: '.$db->error);
     }
     if($result->num_rows===0){
-        throw new Exception('invalid ID: .' $_GET['id'] );
+        throw new Exception('invalid ID: '. $_GET['id'] );
     }
     $postData = $result->fetch_assoc();
+    $postData['comments'] = getCommentsForPost($postData['id']);
     print(json_encode($postData));
+    exit();
 }
-JOIN `comments` AS c
-ON p.`id` = c.`postID` AND c.`status` = 'active'
-c.`userID` AS posterID, c.`added` AS messageAdded, c.`edited` AS messagedEdited, c.`message`
+
 
 
 $query = "SELECT p.`userID`, p.`externalID`, p.`likes`, p.`commentCount`, p.`extension`, p.`originalName`,
