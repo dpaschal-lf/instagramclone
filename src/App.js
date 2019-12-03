f8e17a89b97b3b297607066422bca322dc22fd47
dpaschal-lf <daniel.paschal@gmail.com>
2019-12-02 16:33:25 -0800

    added upload route to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Link} from 'react-router-dom';
 import Gallery from './components/gallery/Gallery.js';
 import GalleryItemDetails from './components/gallery/GalleryItemDetails.js';
 import GalleryItemAdd from './components/gallery/GalleryItemAdd.js';
+import GalleryUpload from './components/gallery/GalleryUpload.js';
 
 class App extends React.Component{
   constructor(props){
@@ -12,10 +13,15 @@ class App extends React.Component{
   render(){
     return(
       <div className="app">
+        <header className="header">
+          <Link to="/upload" className="button add">+</Link>
+        </header>
+        
         <Switch>
           <Route exact path={['/','/gallery']}><Gallery /></Route>
           <Route path="/gallery/add/"><GalleryItemAdd /></Route>
           <Route path="/gallery/:id" component={GalleryItemDetails} />
+          <Route path="/upload" component={GalleryUpload} />
         </Switch>
       </div>
     );
