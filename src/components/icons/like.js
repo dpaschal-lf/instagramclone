433200978017554b321e9be5b9d00a360f7d1e37
dpaschal-lf <daniel.paschal@gmail.com>
2019-12-03 01:04:26 -0800

    added like icon component

---
+++ src/components/icons/like.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class Like extends React.Component{
+    constructor(props){
+        super(props);
+        this.updateLikes = this.updateLikes.bind( this );
+        this.state = {
+            count: props.count
+        }
+    }
+    updateLikes(){
+        fetch('/api/likes.php?'+ this.props.postID)
+            .then( response => response.json() )
+            .then( response => this.setState({ count : this.state.count + response.alterAmount}));
+    }
+    render(){
+        return(
+            <div className="iconContainer" onClick={this.updateLikes}>
+                <i className="fas fa-heart iconBody shadowed"></i>
+                <div className="iconData">{this.state.count}</div>
+            </div>            
+        );
+    }
+}
+
+export default Like;
