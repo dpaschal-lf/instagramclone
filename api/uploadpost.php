4b67c064a3ad8beb0579c6325de74105931720e7
dpaschal-lf <daniel.paschal@gmail.com>
2019-12-02 18:35:46 -0800

    updated endpoint to properly upload file

--- api/uploadpost.php
+++ api/uploadpost.php
@@ -11,8 +11,8 @@ $filePathInfo = pathinfo($_FILES['uploadFile']['name']);
 if(!in_array($filePathInfo['extension'], $acceptableFileExtensions)){
     throw new Exception("{$_FILES['uploadFile']['name']} is an invalid file type.  Can only accept jpg/jpeg, gif, and png");
 }
-print("from: ". $_FILES['uploadFile']['tmp_name']);
-$filePath = "images/{$userExternalID}/{$_FILES['uploadFile']['tmp_name']}.{$filePathInfo['extension']}";
+$newFileName = generateRandomString(20);
+$filePath = "../public/images/{$userExternalID}/{$newFileName}.{$filePathInfo['extension']}";
 if(move_uploaded_file($_FILES['uploadFile']['tmp_name'], $filePath )){
     $uploadFiles[] = $_FILES['uploadFile']['name'];
 } else {
@@ -25,7 +25,7 @@ $internalUserID = getIdFromExternalID( 'users', $userExternalID );
 $query = "INSERT INTO `posts` SET `externalID`= ?, `userID` = ?, `caption` = ?, `likes` = 0, `added` = NOW(), `updated` = NOW(), `commentCount` = 0, `status` = 'active', `extension` = ?";
 
 $result = prepare_statement($query, [
-    $_FILES['uploadFile']['tmp_name'],
+    $newFileName.'.'.$filePathInfo['extension'],
     $internalUserID,
     'caption',
     $filePathInfo['extension']
@@ -34,7 +34,7 @@ $result = prepare_statement($query, [
 if(!$result){
     throw new Exception('error updating post entry ' . $db->error );
 }
-if($result->affected_row===0){
+if($db->affected_rows===0){
     throw new Exception('error uploading data to db');
 }
 
