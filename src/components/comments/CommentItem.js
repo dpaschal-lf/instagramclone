561880dea77703584e74de091525f08d4e3c22bc
dpaschal-lf <daniel.paschal@gmail.com>
2019-12-04 12:55:53 -0800

    large changes for edit changed comment item to class so it can save data added multi mode for display and edit added edit button added save and cancel button

--- src/components/comments/CommentItem.js
+++ src/components/comments/CommentItem.js
@@ -1,21 +1,95 @@
 import React from 'react';
 import UserDisplay from '../userDisplay/UserDisplay.js';
 
-export default props => {
-    return (
-        <div className="commentItem">
-            <UserDisplay  display="small" data={{ 
-                avatar: props.data.avatar, 
-                displayName: props.data.displayName, 
-                joined: props.data.joined 
-            }} id={props.data.userExternalID}/>
-            <div className="messageData">
-                <div className="messageContent">{props.data.message}</div>
-                <div className="messageMetaData">
-                    <div className="messagePosted">{props.data.added}</div>
-                    <div className="messageEdited">{props.data.edited||'NA'}</div>
+class CommentItem extends React.Component{
+    constructor(props){
+        super(props);
+        this.updateField = this.updateField.bind( this );
+        this.save = this.save.bind( this );
+        this.cancel = this.cancel.bind( this );
+        this.beginEdit = this.beginEdit.bind( this );
+        this.state = {
+            mode: 'display',
+            fields: {
+                message: props.data.message
+            }
+        }
+    }
+    beginEdit(){
+        this.setState({
+            mode: 'edit'
+        });
+    }
+    updateField( e ){
+        const element = e.target;
+        const name = e.getAttribute('name');
+        const value = element.value;
+        const newFields = {...this.state.fields};
+        newFields[name] = value;
+        this.setState({
+            fields: newFields
+        });
+    }
+    save(){
+        fetch('/api/commentedit.php',{
+            method: 'put',
+            body: JSON.stringify( this.state.fields )
+        }).then ( ()=>{
+            this.props.updateCallback();
+        })
+    }
+    cancel(){
+        this.setState({
+            fields: {
+                message: this.props.data.message
+            },
+            mode: 'display'
+        })
+    }
+    render(){
+        if( this.state.mode === 'display'){
+            return (
+                <div className="commentItem">
+                    <UserDisplay  display="small" data={{ 
+                        avatar: this.props.data.avatar, 
+                        displayName: this.props.data.displayName, 
+                        joined: this.props.data.joined 
+                    }} id={this.props.data.userExternalID}/>
+                    <div className="messageData">
+                        <div className="messageContent" >{this.props.data.message}</div>
+                        <button onClick={this.beginEdit}>edit</button>
+                        <div className="messageMetaData">
+                            <div className="messagePosted">{this.props.data.added}</div>
+                            <div className="messageEdited">{this.props.data.edited||'NA'}</div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    );
+            );
+        } else {
+            return (
+                <div className="commentItem">
+                    <UserDisplay  display="small" data={{ 
+                        avatar: this.props.data.avatar, 
+                        displayName: this.props.data.displayName, 
+                        joined: this.props.data.joined 
+                    }} id={this.props.data.userExternalID}/>
+                    <div className="messageData">
+                        <textarea className="messageContent" name="message" onChange={this.updateField} defaultValue={this.props.data.message}>
+                        </textarea>
+                        <div className="actionContainer">
+                            <button onClick={this.save}>Save</button>
+                            <button onClick={this.cancel}>Cancel</button>
+                        </div>
+                        <div className="messageMetaData">
+                            <div className="messagePosted">{this.props.data.added}</div>
+                            <div className="messageEdited">{this.props.data.edited||'NA'}</div>
+                        </div>
+                    </div>
+                </div>                
+            );
+        }
+    }
 }
+
+export default CommentItem;
+
