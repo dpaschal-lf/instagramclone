304f0a7e485982756877d1f74913c4aaf49b215e
dpaschal-lf <daniel.paschal@gmail.com>
2019-12-04 12:04:46 -0800

    update of comment count

--- api/commentadd.php
+++ api/commentadd.php
@@ -16,6 +16,10 @@ $internalPostID = getIdFromExternalID( 'posts', $postData['postID'] );
 $internalUserID = getIdFromExternalID( 'users', $userExternalID);
 $externalID = generateRandomString(20);
 
+if(!$db->query('START TRANSACTION')){
+    throw new Exception('problem with transaction start');
+}
+
 $query = "INSERT INTO `comments` SET
         `userID` = ?,
         `postID` = ?,
@@ -34,6 +38,21 @@ $result = prepare_statement($query, [
 
 if(!$result || $db->affected_rows===0){
     throw new Exception('error with query ' . $db->error);
+}
+
+$query = "UPDATE `posts` SET `commentCount` = `commentCount` + 1 WHERE `id`= $internalPostID";
+
+$result = $db->query($query);
+
+if(!$result){
+    throw new Exception('error with post query');
+}
+if($db->affected_rows!==1){
+    throw new Exception('error with post update query');
+}
+
+if(!$db->query('COMMIT')){
+    throw new Exception('problem with transaction start');
 }
 
 print( json_encode( ['success'=>true]));
