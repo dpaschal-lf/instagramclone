b10d91647f4fcf8d13bf524c80496d5c19e2a90f
dpaschal-lf <daniel.paschal@gmail.com>
2019-12-04 23:03:40 -0800

    added check to db email and verify

--- api/login.php
+++ api/login.php
@@ -6,13 +6,28 @@ require_once('mysql_connect.php');
 
 $postData = getBodyData();
 
-if(empty($postData['user'])){
-    throw new Exception('must supply a user');
+if(empty($postData['email'])){
+    throw new Exception('must supply a email');
 }
 if(empty($postData['password'])){
     throw new Exception('must supply a password');
 }
-$internalPostID = getIdFromExternalID( 'posts', $_GET['postID'] );
+$query = "SELECT `id`, `externalID`, `password` FROM `users` WHERE `email`=?";
+
+$result = prepare_statement( $query );
+
+if(!$result){
+    throw new Exception('error with user query');
+}
+
+if( $result->num_rows === 0){
+    throw new Exception('invalid email or password');
+}
+$userData = $result->fetch_assoc();
+
+if( password_verify( $postData['password'], $userData['password'])){
+    
+}
 
 $data = getCommentsForPost( $internalPostID );
 print(json_encode($data));
