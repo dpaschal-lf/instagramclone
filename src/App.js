c30b8e084c7c5a85b10de945cbf7dc543e0c4c9b
dpaschal-lf <daniel.paschal@gmail.com>
2019-12-05 12:44:47 -0800

    added login to app

--- src/App.js
+++ src/App.js
@@ -5,12 +5,23 @@ import Gallery from './components/gallery/Gallery.js';
 import GalleryItemDetails from './components/gallery/GalleryItemDetails.js';
 import GalleryItemAdd from './components/gallery/GalleryItemAdd.js';
 import GalleryUpload from './components/gallery/GalleryUpload.js';
+import Login from './components/connect/Login.js';
 
 class App extends React.Component{
   constructor(props){
     super(props);
+    this.handleUserLogin = this.handleUserLogin.bind( this );
+    this.state = {
+      token: localStorage.getItem('authToken')
+    }
+  }
+  handleUserLogin( token ){
+    localStorage.authToken = token;
   }
   render(){
+    if(this.state.token === null){
+      return <Login success={this.handleUserLogin} failure={()=>{}}/>
+    }
     return(
       <div className="app">
         <header className="header">
