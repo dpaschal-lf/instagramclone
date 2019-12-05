b336672f96ec56ebd66b970242630bd0324605af
dpaschal-lf <daniel.paschal@gmail.com>
2019-12-05 14:10:39 -0800

    put user validation into comment add

--- api/commentadd.php
+++ api/commentadd.php
@@ -4,6 +4,10 @@ require_once('functions.php');
 set_exception_handler('handleExceptions');
 require_once('mysql_connect.php');
 
+if($userData = validateUser()){
+    throw new Exception('must be logged in');
+}
+$userExternalID = $userData['externalID'];
 $postData = getBodyData();
 
 if(empty($postData['postID'])){
@@ -13,7 +17,8 @@ if(empty($postData['commentMessage'])){
     throw new Exception('must supply comment content');
 }
 $internalPostID = getIdFromExternalID( 'posts', $postData['postID'] );
-$internalUserID = getIdFromExternalID( 'users', $userExternalID);
+
+$internalUserID = $userData['id'];
 $externalID = generateRandomString(20);
 
 if(!$db->query('START TRANSACTION')){
@@ -41,7 +46,7 @@ if(!$result || $db->affected_rows===0){
 }
 
 $query = "UPDATE `posts` SET `commentCount` = (`commentCount` + 1) WHERE `id`= $internalPostID";
-print($query);
+
 $result = $db->query($query);
 
 if(!$result){
