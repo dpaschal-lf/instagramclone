cbb15b94c717209d372c52ec832bd680b2f22c98
dpaschal-lf <daniel.paschal@gmail.com>
2019-12-05 16:33:43 -0800

    added methods for hiding / showing modals and for logging out

--- src/components/connect/login.js
+++ src/components/connect/login.js
@@ -5,7 +5,12 @@ class Login extends React.Component{
         super(props);
         this.handleUpdate = this.handleUpdate.bind( this );
         this.handleLogin = this.handleLogin.bind( this );
+        this.handleLogout= this.handleLogout.bind( this );
+        this.showLoginModal = this.showLoginModal.bind( this );
+        this.hideLoginModal = this.hideLoginModal.bind( this );
         this.state = {
+            loggedIn: false,
+            modalShow: false,
             fields: {
                 email: '',
                 password: ''
@@ -23,27 +28,77 @@ class Login extends React.Component{
         })
     }
     handleLogin(){
+        debugger;
         fetch('/api/login.php',{
             method: 'post',
             body: JSON.stringify(this.state.fields)
         })
         .then( response => response.json() )
         .then( response => {
-            this.props.success( response.token );
+            this.props.success( response );
         })
         .catch( ()=>{
             this.props.failure();
         })
     }
+    handleLogout(){
+        fetch('/api/logout.php',{
+            headers: {
+                'auth-token': localStorage.authToken
+            }  
+        })
+        .then( () => {
+            this.props.logout()
+        })
+    }
+    showLoginModal(){
+        this.setState({
+            modalShow: true
+        })
+    }
+    hideLoginModal(){
+        this.setState({
+            modalShow: false
+        })
+    }
     render(){
-        return(
-            <div>
-                <input onChange={this.handleUpdate} defaultValue="daniel.paschal@gmail.com" type="text" name="email" placeholder="user name"/>
-                <input onChange={this.handleUpdate} defaultValue="woohoo" type="password" name="password" placeholder="password"/>
-                <button onClick={this.handleLogin}>Log in</button>
-                <button>Create Account</button>
-            </div>
-        )
+        if(this.state.loggedIn){
+            return(
+                <div className="logout">
+                    <button onClick={this.handleLogout}>logout</button>
+                </div>
+            )
+        }
+        switch(this.props.mode){
+            case 'small':
+                return (
+                    <div className="smallLogin">
+                        <input onChange={this.handleUpdate} defaultValue="daniel.paschal@gmail.com" type="text" name="email" placeholder="user name"/>
+                        <input onChange={this.handleUpdate} defaultValue="woohoo" type="password" name="password" placeholder="password"/>
+                        <button onClick={this.handleLogin}>Log in</button>
+                    </div>
+                )
+            case 'modal':
+                if(this.state.modalShow){
+                    return (<div>Modal</div>)
+                } else{
+                    return (
+                        <button onClick={this.showLoginModal}>Login</button>
+                    )
+                }
+            case  'full':
+            default:
+                return(
+                    <div className="loginPage">
+                        <div className="loginDialog">
+                            <input onChange={this.handleUpdate} defaultValue="daniel.paschal@gmail.com" type="text" name="email" placeholder="user name"/><br/>
+                            <input onChange={this.handleUpdate} defaultValue="woohoo" type="password" name="password" placeholder="password"/><br/>
+                            <button onClick={this.handleLogin}>Log in</button><br/>
+                            <button>Create Account</button>
+                        </div>
+                    </div>
+                )
+        }
     }
 }
 
